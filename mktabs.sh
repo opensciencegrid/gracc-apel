9bc82b470ff0842b07fe0753217040437fc21f78
Carl Edquist <edquist@cs.wisc.edu>
2015-06-17 23:38:27 +0000

    generate tsv's

---
+++ mktabs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")"
+mkdir -p tsv
+
+mktsv () {
+  awk 'NF==2 && /^[^#]/ {print $1,$2}' OFS='\t' "$@"
+}
+
+./pull-oim-tables.sh > tsv/oim-tables.tsv
+mktsv RGMap          > tsv/RGMap.tsv
+mktsv normal_hepspec > tsv/normal_hepspec.tsv
+
