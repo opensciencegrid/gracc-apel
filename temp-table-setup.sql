0b34786fc5c3957781368ce2ab7914b125a1b425
Carl Edquist <edquist@cs.wisc.edu>
2015-06-18 01:27:10 +0000

    text PKs require fixed len, i guess

--- temp-table-setup.sql
+++ temp-table-setup.sql
@@ -2,6 +2,7 @@
 create temporary table oim_resource
 ( site                text
 , resource_group      text
+, resource            text
 , fqdn                text
 , apel_normal_factor  double
 , accounting_name     varchar(256)
@@ -9,19 +10,19 @@ create temporary table oim_resource
 
 -- auxilary lookup tables from files
 create temporary table normal_hepspec
-( resource_group      text
+( resource_group      varchar(255)
 , normal_factor       double
 , PRIMARY KEY (resource_group)
 );
 
 create temporary table rgmap
-( resource            text
+( resource            varchar(255)
 , resource_group      text
 , PRIMARY KEY (resource)
 );
 
 create temporary table volist
-( vo                  text
+( vo                  varchar(255)
 , PRIMARY KEY (vo)
 );
 
@@ -38,8 +39,13 @@ create temporary table gratia_summary
 );
 
 load data local infile 'tsv/oim-tables.tsv'     into table oim_resource;
+-- show warnings;
 load data local infile 'tsv/normal_hepspec.tsv' into table normal_hepspec;
+-- show warnings;
 load data local infile 'tsv/RGMap.tsv'          into table rgmap;
+-- show warnings;
 load data local infile 'tsv/VOList.tsv'         into table volist;
+-- show warnings;
 load data local infile 'tsv/gratia_summary.tsv' into table gratia_summary;
+-- show warnings;
 
