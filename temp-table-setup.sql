6c0294f61de7c33a4fd386f091e5f0723e3bf239
Carl Edquist <edquist@cs.wisc.edu>
2015-06-18 00:57:49 +0000

    include gratia_summary table, so we can join temp tables in one place

--- temp-table-setup.sql
+++ temp-table-setup.sql
@@ -8,25 +8,38 @@ create temporary table oim_resource
 );
 
 -- auxilary lookup tables from files
-create temp table normal_hepspec
+create temporary table normal_hepspec
 ( resource_group      text
 , normal_factor       double
 , PRIMARY KEY (resource_group)
 );
 
-create temp table rgmap
+create temporary table rgmap
 ( resource            text
 , resource_group      text
 , PRIMARY KEY (resource)
 );
 
-create temp table volist
+create temporary table volist
 ( vo                  text
 , PRIMARY KEY (vo)
+);
+
+create temporary table gratia_summary
+( Site             varchar(255)
+, VO               varchar(255)
+, Cores            bigint(20)
+, GlobalUserName   varchar(255)
+, WallDuration     double
+, CpuDuration      double
+, NumberOfJobs     bigint(20)
+, EarliestEndTime  int(10)
+, LatestEndTime    int(10)
 );
 
 load data local infile 'tsv/oim-tables.tsv'     into table oim_resource;
 load data local infile 'tsv/normal_hepspec.tsv' into table normal_hepspec;
 load data local infile 'tsv/RGMap.tsv'          into table rgmap;
 load data local infile 'tsv/VOList.tsv'         into table volist;
+load data local infile 'tsv/gratia_summary.tsv' into table gratia_summary;
 
